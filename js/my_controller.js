0d7a2d22b31fbebc43d52238d4957a5155660f9f
Gonzalo Cervantes <cervgon@gmail.com>
2016-02-05 18:55:15 -0300

    Fix angular issue

--- js/my_controller.js
+++ js/my_controller.js
@@ -2,10 +2,5 @@ myApp.controller('myCtrl', function ($scope) {
 	
 	$scope.hash =  window.location.hash.substr(1);
 	if(!$scope.hash){$scope.hash = 'mulifs--music-library-filesystem'}
-
-
-	$scope.$watch('langCode', function(newValue, oldValue){
-		updateLang(newValue);
-	});
 	
 });
