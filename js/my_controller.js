9bddb7e18218592ee79e8c3bc29ca46bf4d6ddaf
Gonzalo Cervantes <cervgon@gmail.com>
2016-02-24 23:38:56 -0300

    Improve UX

--- js/my_controller.js
+++ js/my_controller.js
@@ -1,6 +1,32 @@
 myApp.controller('myCtrl', function ($scope) {
-	
-	$scope.hash =  window.location.hash.substr(1);
-	if(!$scope.hash){$scope.hash = 'mulifs--music-library-filesystem'}
-	
+    
+    $scope.hash =  window.location.hash.substr(1);
+    if(!$scope.hash){
+        $scope.hash = 'mulifs--music-library-filesystem';
+        $scope.boolChangeClass = false;
+    }
+    else{
+        $scope.boolChangeClass = false;
+    }
+    $('.menu li a').click(function(){
+        $scope.boolChangeClass=true;
+    });
+});
+
+myApp.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+        angular.element($window).bind("scroll", function() {
+             if (this.pageYOffset >= 339) {
+                 scope.boolChangeClass = true;
+             } else {
+                 scope.boolChangeClass = false;
+             }
+             if (this.pageYOffset >= 310) {
+                 scope.fadeoutThis = true;
+             } else {
+                 scope.fadeoutThis = false;
+             }
+            scope.$apply();
+        });
+    };
 });
--- js/panel.js
+++ js/panel.js
@@ -4,5 +4,15 @@ $(document).ready(function(){
 	// Set article height
  	var winH = $(window).height();
  	var footerH = $('#footer').height();
- 	$('.content article').css('min-height',winH-footerH+'px');
+ 	var headerH = $('#header').height();
+ 	$('.content article').css('min-height',winH-footerH-40+'px');
+});
+
+$(function(){
+	$('#container').beforeAfter({
+	animateIntro : true,
+        introDelay : 1500,
+        introDuration : 1000,
+        showFullLinks : false
+	});
 });
