32c2a241fac9286c60eae06a26f17e6f3ad5de54
Danko Miocevic <dankomiocevic@gmail.com>
2016-03-05 18:37:31 -0300

    Create new package music manager.

---
+++ musicmgr/musicFiles.go
@@ -0,0 +1,29 @@
+// Copyright 2016 Danko Miocevic. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Author: Danko Miocevic
+
+// Package tools contains different kind of tools to
+// manage the files in the filesystem.
+// The tools include tools to read the different Tags in the
+// music files and to scan the Directories and SubDirectories in
+// the target path.
+package tools
+
+// FileTags defines the tags found in a specific music file.
+type FileTags struct {
+	Title  string
+	Artist string
+	Album  string
+}
