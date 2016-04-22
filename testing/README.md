59178ebf96df416c33d88d8b805532e54dff3de2
Danko Miocevic <dankomiocevic@gmail.com>
2016-04-22 16:36:03 -0300

    First version of the testing script.

---
+++ testing/README.md
@@ -0,0 +1,44 @@
+MuLiFS testing module
+=====================
+
+The idea behind this testing module is to force MuLiFS to perform all the tasks it would do in a typical environment.
+
+As it is a FileSystem using unit testing was more like forcing a testing tool to something that is not right. To test MuLi it is necessary to initialize many parts and everything works as a group.
+
+I thought (and help me here if you have a better idea) that MuLi should be tested as a FileSystem and the best way to test it is creating a Bash script since it is the tool that will most probably manage MuLi.
+
+So I created a really long script that does all the things that MuLi would do in a real environment, those are listed in the following sections.
+
+Please feel free to add more testing code to the script and help me to improve this FileSystem, just send me a Pull Request.
+
+
+How it works
+------------
+There is an MP3 file that I created (some time ago for a background music in a game) and that file will be modified (change the Tags) in order to generate more files.
+
+Then the file will be copied around and modified doing all the necessary testing and checks.
+
+
+Testing modules
+---------------
+The script will test the following:
+
+- Create lots of mp3 files and add different Tags in order to force MuLi to create the directory structure.
+- Check the .description files.
+- Test the Copy command (Artists, Albums and songs).
+- Test the Rename command (Artists, Albums and songs).
+- Test the Delete command (Artists, Albums and songs).
+- Test the Drop directory (throw new files and existing files).
+- Test the Playlist Rename command (Artists, Albums and songs).
+- Test the Playlist Copy command (Artists, Albums and songs).
+- Test the Playlist Delete command (Artists, Albums and songs).
+
+
+Requirements
+------------
+The requirements to make this tool work are the following:
+
+- Have a MuLiFS compiled binary.
+- Have a ID3Tag tool installed.
+
+I am using MAC default id3 tool and the script works with that, if you are using a different one, there are two funcions in the script (set_tags and check_tags) that need to be modified in order to use the new tool.
---
+++ testing/testMuLi.sh
@@ -0,0 +1,192 @@
+# Copyright 2016 Danko Miocevic. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Danko Miocevic
+
+# This script tests the MuLi filesystem.
+# For more information please read the README.md contained on 
+# this folder.
+
+# Config options
+MULI_X='../mulifs'
+SRC_DIR='./testSrc'
+DST_DIR='./testDst'
+PWD_DIR=$(pwd)
+TEST_SIZE=2
+
+# Tag setting function
+# This function is used to set the tags to the MP3 files.
+# The first argument is the file.
+# The second argument is the Artist.
+# The third argument is the Album.
+# The fourth argument is the Title.
+# This function can be modified in order to use another
+# tag editor command.
+# Returns the id3 command return value.
+function set_tags {
+  id3tag --artist=$2 --album=$3 --song=$4 $1 > /dev/null
+  return $?
+}
+
+# Tag checking function
+# This function is used to check that the tags are correct
+# in a specific MP3 file.
+# The first argument is the file.
+# The second argument is the Artist.
+# The third argument is the Album.
+# The fourth argument is the Title.
+# This function can be modified in order to use another
+# tag editor command.
+# Returns 0 if the tags are correct.
+function check_tags {
+  local ARTIST=$(id3info $1 | grep TPE1)
+  local ALBUM=$(id3info $1 | grep TALB)
+  local TITLE=$(id3info $1 | grep TIT2)
+  if [ ${ARTIST#*:} -ne $2 ]; then
+    return 1
+  fi
+
+  if [ ${ALBUM#*:} -ne $3 ]; then
+    return 2
+  fi
+
+  if [ ${TITLE#*:} -ne $4 ]; then
+    return 3
+  fi
+  return 0
+}
+
+# Create fake MP3s function
+function create_fake {
+  cd $PWD_DIR
+  echo "Creating lots of fake files!"
+  local ARTIST_COUNT=$TEST_SIZE
+
+  while [ $ARTIST_COUNT -gt 0 ]; do
+    echo "Creating ARTIST GreatArtist$ARTIST_COUNT"
+    local ALBUM_COUNT=$TEST_SIZE
+    while [ $ALBUM_COUNT -gt 0 ]; do
+      local SONG_COUNT=$TEST_SIZE
+      while [ $SONG_COUNT -gt 0 ]; do
+        cp "test.mp3" "$SRC_DIR/testAr${ARTIST_COUNT}Al${ALBUM_COUNT}Sn${SONG_COUNT}.mp3"
+        set_tags "$SRC_DIR/testAr${ARTIST_COUNT}Al${ALBUM_COUNT}Sn${SONG_COUNT}.mp3" "GreatArtist$ARTIST_COUNT" "GreatAlbum$ALBUM_COUNT" "Song$SONG_COUNT"
+        if [ $? -ne 0 ]; then
+          echo "ERROR in file $SRC_DIR/testAr${ARTIST_COUNT}Al${ALBUM_COUNT}Sn${SONG_COUNT}.mp3"
+        fi
+        let SONG_COUNT=SONG_COUNT-1
+      done
+      let ALBUM_COUNT=ALBUM_COUNT-1
+    done
+    let ARTIST_COUNT=ARTIST_COUNT-1
+  done
+}
+
+# Check that all the files were in the right place.
+function check_fake {
+  cd $PWD_DIR
+  echo "Checking the fake files."
+  local ARTIST_COUNT=$TEST_SIZE
+
+  while [ $ARTIST_COUNT -gt 0 ]; do
+    cd $PWD_DIR
+    if [ -d "$DST_DIR/GreatArtist$ARTIST_COUNT" ]; then
+      cd "$DST_DIR/GreatArtist$ARTIST_COUNT"
+      local ALBUM_COUNT=$TEST_SIZE
+      while [ $ALBUM_COUNT -gt 0 ]; do
+        if [ -d "GreatAlbum$ALBUM_COUNT" ]; then
+          cd "GreatAlbum$ALBUM_COUNT"
+          local SONG_COUNT=$TEST_SIZE
+          while [ $SONG_COUNT -gt 0 ]; do
+            if [ -f "Song${SONG_COUNT}.mp3" ]; then
+              if [ ! -s "Song${SONG_COUNT}.mp3" ]; then
+                echo "ERROR: File ${DST_DIR}/GreatArtist${ARTIST_COUNT}/GreatAlbum${ALBUM_COUNT}/Song${SONG_COUNT} has 0 size"
+              fi
+            else
+              echo "ERROR: File ${DST_DIR}/GreatArtist${ARTIST_COUNT}/GreatAlbum${ALBUM_COUNT}/Song${SONG_COUNT} not exists"
+            fi
+            let SONG_COUNT=SONG_COUNT-1
+          done
+          cd ..
+        else 
+          echo "ERROR: Directory ${DST_DIR}/GreatArtist${ARTIST_COUNT}/GreatAlbum${ALBUM_COUNT} not exists"
+        fi
+        let ALBUM_COUNT=ALBUM_COUNT-1
+      done
+    else
+      echo "ERROR: Directory ${DST_DIR}/GreatArtist${ARTIST_COUNT} not exists"
+    fi
+    let ARTIST_COUNT=ARTIST_COUNT-1
+  done
+  
+}
+
+# Pre-Mount function
+function create_dirs {
+  cd $PWD_DIR
+  echo -n "Creating dirs..."
+  mkdir $SRC_DIR $DST_DIR
+  if [ $? -eq 0 ] ; then
+      echo "OK!"
+  else
+    echo "ERROR"
+  fi
+}
+
+# Mount FS function
+function mount_muli {
+  cd $PWD_DIR
+  echo -n "Mounting MuLi filesystem..."
+  # Run MuLi in the background
+  $MULI_X $SRC_DIR $DST_DIR &
+  while [ ! -d "$DST_DIR/drop" ]; do
+    sleep 1
+  done
+  echo "OK!"
+}
+
+# Umount FS function
+function umount_muli {
+  cd $PWD_DIR
+  echo -n "Umounting MuLi filesystem..."
+  # Umount the destination directory
+  umount $DST_DIR
+  if [ $? -eq 0 ] ; then
+    echo "OK!"
+  else
+    echo "ERROR"
+  fi
+}
+
+# Clean everything up
+function clean_up {
+  cd $PWD_DIR
+  echo -n "Cleaning everything up..."
+  # Delete the SRC and DST folders
+  rm -rf $SRC_DIR $DST_DIR muli.db
+  if [ $? -eq 0 ] ; then
+    echo "OK!"
+  else
+    echo "ERROR"
+  fi
+}
+
+# Perform tests
+create_dirs
+create_fake
+mount_muli
+check_fake
+umount_muli
+clean_up
+
+
